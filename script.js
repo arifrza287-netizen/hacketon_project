43dae4f6aed38ed2673e184467b4dde31ba32c4d
arifrza287-netizen <arifrza287@gmail.com>
2026-04-10 13:21:39 +0530

    Step 6: Improved UI alignment and added final polish

--- script.js
+++ script.js
@@ -1,21 +1,38 @@
+let ecoScore = 0;
 function showGuide() {
     let type = document.getElementById("wasteType").value;
     let result = document.getElementById("result");
 
-    if (type === "plastic") {
-    result.innerHTML = "♻ Plastic: Recycle in blue bin.<br>💡 Reuse: Use as plant pot.";
+   if (type === "plastic") {
+    result.innerHTML = "♻️ Plastic: Recycle in blue bin.<br>💡 Reuse: Use as plant pot.";
+    result.style.color = "green";
+
+    ecoScore += 10;
+    document.getElementById("score").innerText = "Eco Score: " + ecoScore + " 🌱";
     }
 
-    else if (type === "metal") {
-       result.innerHTML = "🔩 Metal: Scrap dealer.<br>💡 Reuse: Use for DIY items.";
-    } 
-    else if (type === "ewaste") {
-        result.innerHTML = "⚠ E-Waste: Special centers.";
-    } 
-    else if (type === "organic") {
-        result.innerHTML = "🌱 Compost it.<br>💡 Reuse: Make fertilizer.";
-    } 
-    else {
-        result.innerHTML = "Please select a type.";
+else if (type === "metal") {
+    result.innerHTML = "🔩 Metal: Scrap dealer.<br>💡 Reuse: Use for DIY items.";
+    result.style.color = "green";
+
+    ecoScore += 8;
+    document.getElementById("score").innerText = "Eco Score: " + ecoScore + " 🌱";
     }
+
+else if (type === "ewaste") {
+    result.innerHTML = "⚠️ E-Waste: Special centers.<br>💡 Handle carefully.";
+    result.style.color = "red";
+
+    ecoScore += 12;
+    document.getElementById("score").innerText = "Eco Score: " + ecoScore + " 🌱";
+    }
+
+else if (type === "organic") {
+    result.innerHTML = "🌱 Organic: Compost it.<br>💡 Reuse: Make fertilizer.";
+    result.style.color = "green";
+
+    ecoScore += 6;
+    document.getElementById("score").innerText = "Eco Score: " + ecoScore + " 🌱";
+     }
+
 }
